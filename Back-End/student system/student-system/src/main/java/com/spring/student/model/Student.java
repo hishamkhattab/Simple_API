1d090eb3dfb010fd70e78e36e7aa7d664bd012b6
Hisham Khattab <42070333+hishamkhattab@users.noreply.github.com>
2021-07-02 06:29:18 +0200

    Add Annotation To Student Model

--- Back-End/student system/student-system/src/main/java/com/spring/student/model/Student.java
+++ Back-End/student system/student-system/src/main/java/com/spring/student/model/Student.java
@@ -2,19 +2,44 @@ package com.spring.student.model;
 
 
 import com.spring.student.model.enums.Gender;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.xml.crypto.Data;
+import javax.persistence.*;
+import java.util.Date;
 
+
+@Entity(name = "student")
 public class Student {
 
-    private int id;
+
+    @Id                                            //will give the ID , id characteristics (unique, primary key, ..etc)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)    //to make the ID auto increment
+    @Column(name = "id")
+    private long id;
+
+
+    @Column(name = "full_name")
     private String fullName;
-    private String age;
+
+    @Column(name = "gender")
     private Gender gender;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
+
+    @Column(name = "age")
+    private String age;
+
+    @Column(name = "address")
     private String address;
 
-    private Data dataCreated;
-    private Data dataUpdated;
+    @CreationTimestamp
+    @Column(name = "dateCreated")
+    private Date dateCreated;
+
+    @UpdateTimestamp
+    @Column(name = "dateUpdated")
+    private Date dateUpdated;
 
 }
